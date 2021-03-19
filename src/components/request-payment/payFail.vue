5b57218a09e3ba07cdfb788d4d9424352b9957b6
刘家乐 <2846559537@qq.com>
2021-03-19 09:46:09 +0800

    first commit

--- src/components/request-payment/payFail.vue
+++ src/components/request-payment/payFail.vue
@@ -1,42 +1,35 @@
 <template>
-    <div>
-        <ul style="margin-top: 50px;">
-            <li style="text-align: center">
-                <van-cell-group>
-                    <van-image :src="require('../../static/img/fail.png')" />
-                </van-cell-group>
-            </li>
-            <li style="text-align: center">
-                <van-cell-group>
-                    <van-button type="primary" @click="back">返回首页</van-button>
-                </van-cell-group>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul style="margin-top: 50px;">
+      <li style="text-align: center">
+        <van-image :src="require('../../static/img/fail.png')" />
+      </li>
+      <li style="text-align: center">
+        <van-button type="primary" @click="back">返回首页</van-button>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
-    export default {
-        components: {
-
-        },
-        data() {
-            return {
-                merchantId: this.$route.query.merchantId,
-                subjectType: this.$route.query.subjectType,
-            }
-        },
-        methods: {
-            back() {
-                let _this = this;
-                //window.location.href = 'https://www.jiadaoshouyin.com/ttjdac';
-                _this.$router.push({path: '/',query: {merchantId:_this.merchantId,subjectType:_this.subjectType}})
-            }
-
-        },
-
-    }
+export default {
+  components: {},
+  data() {
+    return {
+      merchantId: this.$route.query.merchantId,
+      subjectType: this.$route.query.subjectType,
+    };
+  },
+  methods: {
+    back() {
+      let _this = this;
+      //window.location.href = 'https://www.jiadaoshouyin.com/ttjdac';
+      _this.$router.push({
+        path: "/",
+        query: { merchantId: _this.merchantId, subjectType: _this.subjectType },
+      });
+    },
+  },
+};
 </script>
 
-<style>
-</style>
-
+<style></style>
--- src/components/request-payment/paySuccess.vue
+++ src/components/request-payment/paySuccess.vue
@@ -1,38 +1,39 @@
 <template>
-    <div>
-        <ul style="margin-top: 50px;">
-            <li style="text-align: center"><h2>支付成功</h2></li>
-            <li  style="text-align: center">
-                <van-cell-group>
-                    <van-button type="primary" @click="back">返回首页</van-button>
-                </van-cell-group
-            ></li>
-        </ul>
-    </div>
+  <div>
+    <ul style="margin-top: 50px;">
+      <li style="text-align: center">
+        <van-image
+          :src="require('../../static/img/success.png')"
+          width="20vw"
+        ></van-image>
+        <h3>支付成功</h3>
+      </li>
+      <li style="text-align: center">
+        <van-button type="primary" @click="back">返回首页</van-button>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
-    export default {
-        components: {
-
-        },
-        data() {
-            return {
-                merchantId: this.$route.query.merchantId,
-                subjectType: this.$route.query.subjectType,
-            }
-        },
-        methods: {
-            back() {
-                let _this = this;
-                //window.location.href = 'https://www.jiadaoshouyin.com/ttjdac';
-                _this.$router.push({path: '/',query: {merchantId:_this.merchantId,subjectType:_this.subjectType}})
-            }
-
-        },
-
-    }
+export default {
+  components: {},
+  data() {
+    return {
+      merchantId: this.$route.query.merchantId,
+      subjectType: this.$route.query.subjectType,
+    };
+  },
+  methods: {
+    back() {
+      let _this = this;
+      //window.location.href = 'https://www.jiadaoshouyin.com/ttjdac';
+      _this.$router.push({
+        path: "/",
+        query: { merchantId: _this.merchantId, subjectType: _this.subjectType },
+      });
+    },
+  },
+};
 </script>
 
-<style>
-</style>
-
+<style></style>
